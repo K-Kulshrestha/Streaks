a2f3d530e3a812be20dddc5a6e0b16c79978ff40
Kshitij Kulshrestha <91348714+K-Kulshrestha@users.noreply.github.com>
2023-09-20 15:01:07 -0500

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,13 @@
+// Sample code for logout functionality
+const logoutButton = document.getElementById('logout-btn');
+const addButton = document.getElementById('add-btn');
+
+logoutButton.addEventListener('click', () => {
+    // Implement your logout logic here
+    alert('Logged out!');
+});
+
+
+addButton.addEventListener('click', async () => {
+    alert('Tried to add');
+});
